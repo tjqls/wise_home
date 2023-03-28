a0a54a74e50ffd1b1c634f72a09a94aba83ad9a4
tjqls <tjqls2013@gmail.com>
2023-03-28 14:33:41 +0900

    4 level

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,11 +12,18 @@ public class App {
 
     public void run() {
         System.out.println("== 명연 앱 ==");
-
+        long count=1;
         while (true){
          String command = sc.nextLine().trim();
          if(command.equals("종료")){
              break;
+         }else if(command.equals("등록")){
+             System.out.println("명언)");
+             String content = sc.nextLine();
+             System.out.println("작가)");
+             String human = sc.nextLine();
+             System.out.println(count+"번 명언이 등록되었습니다.");
+             count++;
          }
         }
 
