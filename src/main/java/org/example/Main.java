bd2475383a0460e53547c0d47d97c64bfece2d23
tjqls <tjqls2013@gmail.com>
2023-03-28 14:23:21 +0900

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc= new Scanner(System.in);
+
+        new App(sc).run();
+
+        sc.close();
     }
 }
